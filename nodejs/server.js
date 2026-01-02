d2295b8c0e9840e91cc52099862495aa1808016e
Eric Goldman <egoldman@salesforce.com>
2026-01-02 10:57:04 -0500

    initial working fixed secret in docker compose file

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,23 +1,66 @@
 require('dotenv').config();
 
+const util = require('util');
+const jsforce = require('jsforce');
+
 const express = require('express');
 const app = express();
 const httpProxy = require('http-proxy');
 
-const proxies = JSON.parse(process.env.PROXY_CONFIGS);
+const proxy_configs = JSON.parse(process.env.PROXY_CONFIGS);
 const proxy = httpProxy.createProxyServer({});
 
-app.all('/:clientId/', (req, res) => {
-    const clientId = req.params['clientId'] || undefined;
-    const config = proxies[clientId];
+const connections = {};
+
+function getConfigByKey(key) {
+    return proxy_configs[key]
+}
+
+async function getSalesforceAccessTokenByConfigKey(config_key) {
+    // get the config object
+    const config = getConfigByKey(config_key)
+    // connect to salesforce org
+    const conn = new jsforce.Connection({
+        instanceUrl: config.instance_url,
+        oauth2: {
+            clientId: config.oauth.client_id,
+            clientSecret: config.oauth.client_secret,
+            loginUrl: config.instance_url
+        },
+        loginUrl: config.login_url
+    });
+    const userInfo = await conn.authorize({ grant_type: "client_credentials" })
+    // get the auth token
+    const access_token = conn.accessToken;
+    return access_token;
+}
+
+proxy.on('proxyReq', async function(proxyReq, req, res, options) {
+});
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+});
+
+app.all('/:config_key/', async (req, res) => {
+    const config_key = req.params['config_key'] || undefined;
+    const config = proxy_configs[config_key];
     if (!config) {
         res.sendStatus(400).end();
     } else {
-        const domain = proxies['local'].domain
-        console.log(`Proxy to ${domain}`)
+        const access_token = await getSalesforceAccessTokenByConfigKey(config_key);
+        if (access_token) {
+            req.headers['Authorization'] = `Bearer ${access_token}`;
+            req.headers['X-Proxy-Auth'] = access_token;
+        }
+        const proxy_target = proxy_configs[config_key].proxy_target
+        console.log(`Proxy to ${proxy_target}`)
         proxy.web(req, res, {
-            target: domain,
-            secure: false
+            target: proxy_target,
+            secure: false,
+            ignorePath: true,
+            changeOrigin: true
+        }, (e) => {
+            console.error(e);
         });
     }
 });
