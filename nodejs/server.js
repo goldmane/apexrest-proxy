e275229bfcafbd842a86ce43ed22a398a3c8aa1c
Eric Goldman <goldmanej@gmail.com>
2026-01-01 22:40:12 -0500

    initial

---
+++ nodejs/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const httpProxy = require('http-proxy');
+
+const proxies = JSON.parse(process.env.PROXY_CONFIGS);
+const proxy = httpProxy.createProxyServer({});
+
+app.all('/:clientId/', (req, res) => {
+    const clientId = req.params['clientId'] || undefined;
+    const config = proxies[clientId];
+    if (!config) {
+        res.sendStatus(400).end();
+    } else {
+        const domain = proxies['local'].domain
+        console.log(`Proxy to ${domain}`)
+        proxy.web(req, res, {
+            target: domain,
+            secure: false
+        });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
