f46b5d4587aa8973d757fb806fc508252a440c3f
Eric Goldman <goldmanej@gmail.com>
2026-01-10 22:11:14 -0500

    Initial java template

---
+++ java/src/main/java/com/ericjgoldman/salesforce/apexrestproxy/ApexRestProxyApplication.java
@@ -0,0 +1,13 @@
+package com.ericjgoldman.salesforce.apexrestproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexRestProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexRestProxyApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/ericjgoldman/salesforce/apexrestproxy/ApexRestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericjgoldman.salesforce.apexrestproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexRestProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
